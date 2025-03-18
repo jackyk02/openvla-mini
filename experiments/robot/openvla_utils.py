41ece7896ccd7b49aea2d8324985a4966510cfd3
jackyk02 <jackykwok@berkeley.edu>
2025-03-18 04:47:23 +0000

    reward can only take batch of 2

--- experiments/robot/openvla_utils.py
+++ experiments/robot/openvla_utils.py
@@ -102,7 +102,7 @@ def get_rewards(instruction, image_path, actions):
     all_rewards = []
     
     # Process actions in batches of 4
-    batch_size = 4
+    batch_size = 2
     num_batches = math.ceil(len(actions) / batch_size)
     
     for i in range(num_batches):
