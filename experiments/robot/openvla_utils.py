e03de7e85e85e38f447df157ddc20559748d70ef
Jacky Kwok <77720778+jackyk02@users.noreply.github.com>
2025-04-19 22:26:03 -0700

    Update openvla_utils to use same image VLA for RM

--- experiments/robot/openvla_utils.py
+++ experiments/robot/openvla_utils.py
@@ -602,7 +602,7 @@ def get_vla_action(vla, processor, base_vla_name, obs, task_label, unnorm_key, c
     
     print(output_ids)
 
-    reward_img = "/root/openvla-mini/transfer_images/reward_img.jpg"
+    reward_img = "/root/openvla-mini/transfer_images/vla_processed_img.jpg"
     rewards = get_rewards(instruction, reward_img, output_ids)
     selected_index = np.argmax(rewards)
 
